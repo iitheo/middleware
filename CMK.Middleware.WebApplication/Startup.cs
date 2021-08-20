d268f4650329422cbcfb28b0bda58e0cc005f7ec
Theodore Kamma <iitheo@yahoo.com>
2021-08-20 05:47:18 +0100

    add custom middleware displaying text to web page

--- CMK.Middleware.WebApplication/Startup.cs
+++ CMK.Middleware.WebApplication/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,10 +22,10 @@ namespace CMK.Middleware.WebApplication
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-        }
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddRazorPages();
+        //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -40,17 +41,30 @@ namespace CMK.Middleware.WebApplication
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) => 
             {
-                endpoints.MapRazorPages();
+                await context.Response.WriteAsync("<p>From middleware 1<p>");
+                await next();
             });
+
+            app.Use(async (context, next) => 
+            {
+                await context.Response.WriteAsync("<p>from middleware 2</p>");
+                await next();
+            });
+
+
+            //app.UseHttpsRedirection();
+            //app.UseStaticFiles();
+
+            //app.UseRouting();
+
+            //app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapRazorPages();
+            //});
         }
     }
 }
