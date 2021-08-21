34b06e8aaa4326b1447e3d9ba0d17b1d8e6a516a
Theodore Kamma <iitheo@yahoo.com>
2021-08-21 13:55:00 +0100

    add api controller

--- CMK.Middleware.WebApplication/Startup.cs
+++ CMK.Middleware.WebApplication/Startup.cs
@@ -22,10 +22,12 @@ namespace CMK.Middleware.WebApplication
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //    services.AddRazorPages();
-        //}
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddRazorPages();
+            services.AddControllers();
+            services.AddLogging();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -41,57 +43,58 @@ namespace CMK.Middleware.WebApplication
                 app.UseHsts();
             }
 
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("<p>From middleware 1<p>");
-                await next();
-            });
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("<p>From middleware 1<p>");
+            //    await next();
+            //});
 
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("<p>From middleware 2</p>");
-                await next();
-            });
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("<p>From middleware 2</p>");
+            //    await next();
+            //});
 
-            app.MapWhen(context => context.Request.Query.ContainsKey("name"), action => {
-                action.Run(async context => {
-                    var name = context.Request.Query["name"];
-                    await context.Response.WriteAsync($"<p>Hello {name}!</p>");
-                });
-            });
+            //app.MapWhen(context => context.Request.Query.ContainsKey("name"), action => {
+            //    action.Run(async context => {
+            //        var name = context.Request.Query["name"];
+            //        await context.Response.WriteAsync($"<p>Hello {name}!</p>");
+            //    });
+            //});
 
-            app.Map("/mysite",action => {
-                action.Use(async (context,next) => {
-                    await context.Response.WriteAsync("<p>Hello from</p>");
-                    await next();
-                });
-                action.Use(async (context, next) => {
-                    await context.Response.WriteAsync("<p>my other site.</p>");
-                    await next();
-                });
-                action.Run(async context => {
-                    await context.Response.WriteAsync("<p>End of mysite</p>");
-                });
-            });
-            
-
-            app.Run(async context => 
-            {
-                await context.Response.WriteAsync("<p>From RUN middleware</p>");
-            });
+            //app.Map("/mysite",action => {
+            //    action.Use(async (context,next) => {
+            //        await context.Response.WriteAsync("<p>Hello from</p>");
+            //        await next();
+            //    });
+            //    action.Use(async (context, next) => {
+            //        await context.Response.WriteAsync("<p>my other site.</p>");
+            //        await next();
+            //    });
+            //    action.Run(async context => {
+            //        await context.Response.WriteAsync("<p>End of mysite</p>");
+            //    });
+            //});
 
 
-            //app.UseHttpsRedirection();
+            //app.Run(async context => 
+            //{
+            //    await context.Response.WriteAsync("<p>From RUN middleware</p>");
+            //});
+
+
+            app.UseHttpsRedirection();
             //app.UseStaticFiles();
 
-            //app.UseRouting();
+            app.UseRouting();
 
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapRazorPages();
-            //});
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
         }
     }
 }
