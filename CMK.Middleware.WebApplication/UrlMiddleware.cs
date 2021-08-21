6f1e952a4b9dfa01d02f527c35e136342ef6a7c3
Theodore Kamma <iitheo@yahoo.com>
2021-08-21 14:07:53 +0100

    add custom middleware class

---
+++ CMK.Middleware.WebApplication/UrlMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMK.Middleware.WebApplication
+{
+    public class UrlMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public UrlMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+    }
+}
